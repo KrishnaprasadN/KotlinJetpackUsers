3227b4e2d917ec3b867410fc1d16c682dec42752
Krishnaprasad N <krishnaprasad.n@glowtouch.com>
2020-03-13 14:51:26 +0530

    Committing the app skeleton

---
+++ app/src/main/java/com/devteam/jetpackusers/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.devteam.jetpackusers.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devteam.jetpackusers.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
