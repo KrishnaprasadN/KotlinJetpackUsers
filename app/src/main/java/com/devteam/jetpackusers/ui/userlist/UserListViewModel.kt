e254cceb30699becb4fbae7fdf3dc4b671a2da11
Rakshith Raj <rakshith.r@CORP.SYSTECHMNG.NET>
2020-03-16 18:27:10 +0530

    Resolved confict

--- app/src/main/java/com/devteam/jetpackusers/ui/userlist/UserListViewModel.kt
+++ app/src/main/java/com/devteam/jetpackusers/ui/userlist/UserListViewModel.kt
@@ -6,15 +6,6 @@ import androidx.lifecycle.ViewModel
 import com.devteam.jetpackusers.io.model.User
 
 class UserListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-    val  users: LiveData<List<User>> = MutableLiveData<List<User>>()
-
-import androidx.lifecycle.liveData
-import com.devteam.jetpackusers.io.model.User
-import com.devteam.jetpackusers.repository.DataRepository
-import androidx.lifecycle.liveData
-
-class UserListViewModel : ViewModel() {
     val dataRepository = DataRepository()
 
     /*val users = liveData {
